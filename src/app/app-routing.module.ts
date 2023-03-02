1cb797fd59d5351b4670449698e867d9c2e1aa60
AlaCoder <126266236+AlaCoder@users.noreply.github.com>
2023-03-02 12:02:25 +0100

    changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdministrationComponent } from './components/administration/administration.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { FacturationComponent } from './components/facturation/facturation.component';
+import { FraisComponent } from './components/frais/frais.component';
+import { MattersComponent } from './components/matters/matters.component';
+import { ReportingComponent } from './components/reporting/reporting.component';
+import { TimesheetsComponent } from './components/timesheets/timesheets.component';
+import { ValidationComponent } from './components/validation/validation.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: DashboardComponent},
+  {path: 'dashboard',component: DashboardComponent},
+  {path: 'timesheets',component: TimesheetsComponent},
+  {path: 'facturation',component: FacturationComponent},
+  {path: 'frais',component: FraisComponent},
+  {path: 'validation',component: ValidationComponent},
+  {path: 'reporting',component: ReportingComponent},
+  {path: 'matters',component: MattersComponent},
+  {path: 'clients',component: ClientsComponent},
+  {path: 'administration',component: AdministrationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ValidationComponent } from './components/validation/validation.component';
+import { MattersComponent } from './components/matters/matters.component';
+import { TimesheetsComponent } from './components/timesheets/timesheets.component';
+import { FraisComponent } from './components/frais/frais.component';
+import { AdministrationComponent } from './components/administration/administration.component';
+import { ReportingComponent } from './components/reporting/reporting.component';
+import { FacturationComponent } from './components/facturation/facturation.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './layouts/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    ClientsComponent,
+    ValidationComponent,
+    MattersComponent,
+    TimesheetsComponent,
+    FraisComponent,
+    AdministrationComponent,
+    ReportingComponent,
+    FacturationComponent,
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
