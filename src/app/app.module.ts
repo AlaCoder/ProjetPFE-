6b63092335526af5e3d2001349f2ebba75af3cc8
rayen573 <rayeharbaoui@gmail.com>
2023-03-05 15:43:15 +0100

    theni marra

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ValidationComponent } from './components/validation/validation.component';
+import { MattersComponent } from './components/matters/matters.component';
+import { TimesheetsComponent } from './components/timesheets/timesheets.component';
+import { FraisComponent } from './components/frais/frais.component';
+import { AdministrationComponent } from './components/administration/administration.component';
+import { ReportingComponent } from './components/reporting/reporting.component';
+import { FacturationComponent } from './components/facturation/facturation.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { ProfilComponent } from './profil/profil.component';
+import { LoginComponent } from './login/login.component';
+import { PagesRegisterComponent } from './pages-register/pages-register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    ClientsComponent,
+    ValidationComponent,
+    MattersComponent,
+    TimesheetsComponent,
+    FraisComponent,
+    AdministrationComponent,
+    ReportingComponent,
+    FacturationComponent,
+    DashboardComponent,
+    HeaderComponent,
+    ProfilComponent,
+    LoginComponent,
+    PagesRegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
